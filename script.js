0d6363dababa3c3767750bd438d95035a481c376
axel engblom <axelengblom94@gmail.com>
2021-02-02 10:56:54 +0100

    added jump and falling down function

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 let bg = document.getElementById("bg");
 let bg2 = document.getElementById("bg2");
+let box = document.getElementById("box");
 let counter = 0;
 let right = 0;
-let timer = setInterval(function(){
+let timer = setInterval(function () {
+  if (flag == true) {
+    jump();
+    flag = false;
+  }
+}, 500);
+let jumpUp = 500;
+let flag = false;
 
+let jump = function () {
+  console.log("bird");
+};
 
-},50);
+document.addEventListener("keyup", function (evt) {
+  console.log(evt);
+  switch (evt.key) {
+    case "ArrowUp":
+      //   setInterval(function () {
+      console.log(evt);
+      jumpUp += 10;
+      box.style.bottom = jumpUp + "px";
+      flag = true;
+    //   }, 500);
+  }
+});
 
-let jump = function(){
-    
+function down() {
+  setInterval(function () {
+    jumpUp -= 5;
+    box.style.bottom = jumpUp + "px";
+    flag = true;
+  }, 100);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+jump();
+down();
 /*let movebg = function(){
     counter ++;
     right +=10;
