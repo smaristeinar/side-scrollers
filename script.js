eae739187d39b6f17a0e85b93fb108dcf9547656
smari steinar <smarikul@hotmail.com>
2021-02-02 10:25:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let bg = document.getElementById("bg");
+let bg2 = document.getElementById("bg2");
+let counter = 0;
+let right = 0;
+let timer = setInterval(function(){
+
+
+},50);
+
+let jump = function(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let movebg = function(){
+    counter ++;
+    right +=10;
+    if (right == screen.width) {
+        right = -screen.width + "px";
+        
+
+    }
+    bg.style.right = right + "px";
+}
+ */
