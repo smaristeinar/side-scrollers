17d2ccbfe6dec5611b5cd670572d195b9de9a56b
axel engblom <axelengblom94@gmail.com>
2021-02-03 10:53:09 +0100

    added the top obstacles and made falling smoother

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 let bg = document.getElementById("bg");
 let box = document.getElementById("box");
 let obj1 = document.getElementById("obs1");
-const obstacles = document.getElementById('obstacles');
+const obstacles = document.getElementById("obstacles");
 let windowWidth = 800;
 
 let counter = 0;
@@ -10,18 +10,14 @@ let right = 0;
 
 let jumpUp = 500;
 
-
-
 function graivty() {
   setInterval(function () {
-if (jumpUp > 0) {
-  jumpUp -= 5;
-  box.style.bottom = jumpUp + "px";
+    if (jumpUp > 0) {
+      jumpUp -= 1;
+      box.style.bottom = jumpUp + "px";
+    }
+  }, 15);
 }
-  },
-   100);
-}
-
 
 // let moveObs = function(){
 //   counter ++;
@@ -30,70 +26,91 @@ if (jumpUp > 0) {
 //       right = -windowWidth + "px";
 //       obj1.style.right = "100px";
 //       right = 0;
-//     } 
+//     }
 //   obj1.style.right = right + "px";
 //   colCheck();
 // }
 
-function move(){
+function move() {
   setInterval(function () {
-    moveObs();
-    console.log()
+    // moveObs();
+    console.log();
   }, 100);
 }
 
-let colCheck = function(){
-    if (right == windowWidth-300 && jumpUp < 400) {
-        console.log("gameover");
-    }
-}
+let colCheck = function () {
+  if (right == windowWidth - 300 && jumpUp < 400) {
+    console.log("gameover");
+  }
+};
 
 document.addEventListener("keyup", function (evt) {
-    if (evt.key == "ArrowUp"){   
-      jumpUp += 30;
-      box.style.bottom = jumpUp + "px";
-    }
-  });
+  if (evt.key == "ArrowUp") {
+    jumpUp += 30;
+    box.style.bottom = jumpUp + "px";
+  }
+});
 
-function createObstacle () {
-  const ob = document.createElement('div');
-  ob.id = 'ob' + obCounter;
-  ob.classList.add('obstacle');
+function createObstacle() {
+  const ob = document.createElement("div");
+  const obTop = document.createElement("div");
+  ob.id = "ob" + obCounter;
+  obTop.id = "obTop" + obCounter;
+  ob.classList.add("obstacle");
+  obTop.classList.add("obstacleTop");
   const obId = ob.id;
+  const obTopId = obTop.id;
   const height = 50 + Math.floor(Math.random() * 200);
   const width = 10 + Math.floor(Math.random() * 20);
-  ob.style.height = height + 'px';
-  ob.style.width = width + 'px';
-  
+  ob.style.height = height + "px";
+  ob.style.width = width + "px";
+  const obTopHeight = 1000;
+
+  obTop.style.bottom = height + 100 + "px";
+  obTop.style.height = obTopHeight + "px";
+  obTop.style.width = width + "px";
+
   obCounter++;
   obstacles.appendChild(ob);
-  moveObstacles(obId, height, width);
+  obstacles.appendChild(obTop);
+  moveObstacles(obId, height, width, obTopId, obTopHeight);
 }
 
-function moveObstacles (obId, height, width) {
+// function createObstacleTop() {
+//   const ob = document.createElement("div");
+//   ob.id = "ob" + obCounter;
+//   ob.classList.add("obstacleTop");
+//   const obId = ob.id;
+//   const height = 50 + Math.floor(Math.random() * 200);
+//   const width = 10 + Math.floor(Math.random() * 20);
+//   ob.style.height = height + "px";
+//   ob.style.width = width + "px";
+
+//   obCounter++;
+//   obstacles.appendChild(ob);
+//   moveObstacles(obId, height, width);
+// }
+
+function moveObstacles(obId, height, width, obTopId, obTopHeight) {
   let right = 0;
-  let timer = setInterval(function() {
+  let timer = setInterval(function () {
     const ob = document.getElementById(obId);
+    const obTop = document.getElementById(obTopId);
     right++;
-    ob.style.right = right + 'px';
+    ob.style.right = right + "px";
+    obTop.style.right = right + "px";
     //testCollision(odId, height, width, timer);
     if (right == 300) {
       createObstacle();
     }
     if (right > 799) {
       ob.remove();
+      obTop.remove();
       clearInterval(timer);
     }
   }, 10);
 }
 
-
-move();
+// move();
 graivty();
 createObstacle();
-
-
-
-
-
-
